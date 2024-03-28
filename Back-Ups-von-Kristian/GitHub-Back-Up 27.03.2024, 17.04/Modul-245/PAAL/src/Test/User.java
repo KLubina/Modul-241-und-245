7aacc831a17cd586a1981a2b17e68e1e8fc9c0f0
KLubina <155258933+KLubina@users.noreply.github.com>
2024-03-28 08:52:53 +0100

    Add files via upload

---
+++ Back-Ups-von-Kristian/GitHub-Back-Up 27.03.2024, 17.04/Modul-245/PAAL/src/Test/User.java
@@ -0,0 +1,19 @@
+package Test;
+
+import java.io.Serializable;
+
+public class User implements Serializable {
+    private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ Back-Ups-von-Kristian/GitHub-Back-Up 27.03.2024, 17.04/Modul-245/testLukas/src/main/java/githubTest/TestKlasse.java
@@ -0,0 +1,5 @@
+package githubTest;
+
+public class TestKlasse {
+
+}
